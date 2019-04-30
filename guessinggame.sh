f3320f0808e85c01a6e33c2b629815b2104a3ae6
sam <samuel.t.giles@gmail.com>
2019-04-30 12:25:24 +0800

    add catch for non-number input

--- guessinggame.sh
+++ guessinggame.sh
@@ -5,7 +5,19 @@ function guessinggame {
     guess=0
     file_count=$(ls . | wc -l)
 
-    echo $file_count
+    echo "Guess how many files are in the current directory:"
+
+    while [[ $guess -ne $file_count ]]
+    do
+        read guess
+
+        if [[ $guess =~ [^0-9] ]]
+        then
+            echo "Please enter a positive number:"
+            # reset $guess to a number to prevent errors in the while loop
+            guess=0
+        fi
+    done
 }
 
 guessinggame
