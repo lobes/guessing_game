d4fd41b3a4fab0700d7a31a88448f7099ef3dc8b
sam <samuel.t.giles@gmail.com>
2019-04-30 12:32:15 +0800

    add final guess script

--- guessinggame.sh
+++ guessinggame.sh
@@ -16,6 +16,14 @@ function guessinggame {
             echo "Please enter a positive number:"
             # reset $guess to a number to prevent errors in the while loop
             guess=0
+        elif [[ $guess -lt $file_count ]]
+        then
+            echo "Too low. Have another crack:"
+        elif [[ $guess -gt $file_count ]]
+        then
+            echo "Too high. Have another crack:"
+        else
+            echo "Well done! Have a gold star."
         fi
     done
 }
